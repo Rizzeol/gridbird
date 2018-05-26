f2e6d43a74c5d23233d340c9da88a8b86d3fdc43
mu7lan <mu7lan@msn.com>
2018-05-26 17:31:33 +0200

    Consumer

--- gridbird.py
+++ gridbird.py
@@ -5,22 +5,36 @@ class House:
     def __init__(self, houseName):
         self.houseName = houseName
         self.batteryState = 100
-        self.session_stopping = False #stops the battery charge
+
+        self.session_stopping = False #stops the battery supply
         self.battery_thread = None
         self.demand = 0 #No demand nessesary
 
-    def keepalive(self):
+    def supplyPower(self):
         ticks = 0
         while True:
-            print('================keepalive=================')
-            print('battery:', self.batteryState)
+            print('%s ---- supplying-power----- battery %s'%(self.houseName,self.batteryState)) #the battery can supply power to the grid
             self.batteryState = self.batteryState-1
             time.sleep(1)
             if (self.session_stopping == True):
                  break
 
-    def start_keepalive_thread(self):
-        self._msghandler_thread = threading.Thread(target=self.keepalive)
+    def start_supply_power_thread(self):
+        self._msghandler_thread = threading.Thread(target=self.supplyPower)
+        self._msghandler_thread.setDaemon(True)
+        self._msghandler_thread.start()
+
+    def consumePower(self):
+        ticks = 0
+        while True:
+            print('%s---- consume-power----- battery: %s'%(self.houseName,self.batteryState)) #the battery can supply power to the grid
+            self.batteryState = self.batteryState+1
+            time.sleep(1)
+            if (self.session_stopping == True):
+                 break
+
+    def start_consume_power_thread(self):
+        self._msghandler_thread = threading.Thread(target=self.consumePower)
         self._msghandler_thread.setDaemon(True)
         self._msghandler_thread.start()
 
@@ -31,16 +45,22 @@ def DataPoints(randomBaseInt):
         dirList = []
 
 def testHouseBidding():
-    house = House('H1')
-    house.batteryState = 100
+    house1 = House('H1')
+    house1.batteryState = 100
     randomData = DataPoints(1)
-    house.start_keepalive_thread()
-    time.sleep(5)
-    house.stop_supply()
+    house1.start_supply_power_thread()
 
+    house2 = House('H2')
+    house2.batteryState = 50
+    randomData = DataPoints(1)
+    house2.start_consume_power_thread()
+    time.sleep(5)
+    house1.stop_supply()
 
 if __name__ == "__main__":
     # Test all Houses and print its batterystatus
+    #  print energy demand
+
     testHouseBidding()
 
 
