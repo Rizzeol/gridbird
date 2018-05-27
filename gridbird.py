b86a07c3ff4c90f41d4d7b91c2992eb3af4792f3
Rohit Paudel <rohitpaudel9@gmail.com>
2018-05-27 11:03:24 +0200

    We have four data points for each houses. They are : 1. Sell_Price (Price at which each house want to sell their stored energy if found perfect match to trade ) 2. Buy_price (Price at which each house want to buy energy if found perfect match to trade )
    3. Demand (1 if house has demand and 0 if no demand)
    4. Storage (Battery percent of each houses)
    
    For testing purpose, I have created random data points for each data types mentioned above and stored them in seperate text files.

--- gridbird.py
+++ gridbird.py
@@ -1,5 +1,7 @@
 import time
 import threading
+import random
+
 
 class House:
     def __init__(self, houseName):
@@ -41,18 +43,72 @@ class House:
     def stop_supply(self):
         self.session_stopping = True
 
-def DataPoints(randomBaseInt):
-        dirList = []
+# test data for selling price for each houses
+def sell_p(house_num):
+        try:
+            with open('sellprice.txt', 'w+') as myfile:
+                for i in range(house_num):
+                    p_s = round((random.uniform(0.002, 0.008)),4)
+                    p_s = str(p_s)
+
+                    myfile.write((p_s + "\n"))
+        except IOError:
+            print("Error opening file")
+        finally:
+            myfile.close()
+# test data for buying price for each houses
+def buy_p(house_num):
+        try:
+            with open('buyprice.txt', 'w+') as myfile:
+                for i in range(house_num):
+                    p_b = round((random.uniform(0.002, 0.008)), 4)
+                    p_b = str(p_b)
+                    myfile.write((p_b + "\n"))
+        except IOError:
+            print("Error opening file")
+        finally:
+            myfile.close()
+#Battery percentage of each houses
+# Some houses may not have battery, have to fix that as well.
+def storage(house_num):
+
+        try:
+            with open('storage.txt', 'w+') as myfile:
+                for i in range(house_num):
+                    stor = random.randint(7,97)
+                    stor = str(stor)
+                    myfile.write((stor + "\n"))
+
+        except IOError:
+            print("Error opening file")
+        finally:
+            myfile.close()
+# representating demand data as 0 or 1 for each houses
+def demand(house_num):
+    try:
+        with open('demand.txt', 'w+') as myfile:
+            for i in range(house_num):
+                list = [0,1]
+                random.shuffle(list)
+                dem = random.choice(list)
+                dem = str(dem)
+                print(dem)
+                myfile.write((dem + "\n"))
+
+    except IOError:
+        print("Error opening file")
+    finally:
+        myfile.close()
 
 def testHouseBidding():
     house1 = House('H1')
     house1.batteryState = 100
-    randomData = DataPoints(1)
+    #randomData = DataPoints(5)
     house1.start_supply_power_thread()
 
     house2 = House('H2')
     house2.batteryState = 50
-    randomData = DataPoints(1)
+    #randomData = DataPoints(5)
     house2.start_consume_power_thread()
     time.sleep(5)
     house1.stop_supply()
@@ -60,7 +116,13 @@ def testHouseBidding():
 if __name__ == "__main__":
     # Test all Houses and print its batterystatus
     #  print energy demand
-
+    sell_p(5)
+    buy_p(5)
+    storage(5)
+    demand(5)
     testHouseBidding()
+
+
+
 
 
